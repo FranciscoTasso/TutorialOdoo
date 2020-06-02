2a41ddf60358a84bf39dfa2a66a65562613cf8f7
FranciscoTasso <franciscotasso@hotmail.com>
2020-06-02 11:50:03 -0300

    Tutorial

--- __manifest__.py
+++ __manifest__.py
@@ -21,14 +21,15 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'board'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         #'views/templates.xml',
         'views/openacademy.xml',
-        'reports.xml'
+        'reports.xml',
+        # 'views/session_board.xml',
         #'views/partner.xml'
     ],
     # only loaded in demonstration mode
